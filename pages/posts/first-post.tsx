c4b8f3f8e38ed8ecd62aed97e4f3d47658c006dc
Fumio Nonaka <fumio@mx10.ttcn.ne.jp>
2023-01-19 19:52:29 +0900

    Next入門02 イメージとメタデータおよびCSSを扱う

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,14 +1,15 @@
-import Link from 'next/link';
+import Head from 'next/head';
 import type { FC } from 'react';
+import Layout from '../../components/layout';
 
 const FirstPost: FC = () => {
 	return (
-		<>
+		<Layout>
+			<Head>
+				<title>First Post</title>
+			</Head>
 			<h1>First Post</h1>
-			<h2>
-				<Link href="/">← Back to home</Link>
-			</h2>
-		</>
+		</Layout>
 	);
 };
 export default FirstPost;
