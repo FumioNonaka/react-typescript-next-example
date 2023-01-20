0e072fe65c626c0295cb2f2f95d8e032b550b9f4
Fumio Nonaka <fumio@mx10.ttcn.ne.jp>
2023-01-20 15:46:25 +0900

    Next入門03 プリレンダリングとデータ取得”

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
 import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
+import { getSortedPostsData } from '../lib/posts';
+import type { PostData } from '../lib/posts';
 import utilStyles from '../styles/utils.module.css';
 
-export default function Home() {
+type Props = {
+	allPostsData: PostData[];
+};
+export const getStaticProps = async () => {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+};
+export default function Home({ allPostsData }: Props) {
 	return (
 		<Layout home>
 			<Head>
@@ -14,6 +27,20 @@ export default function Home() {
 					(This is a sample website - you’ll be building a site like this on{' '}
 					<a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
 				</p>
+			</section>
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h2 className={utilStyles.headingLg}>Blog</h2>
+				<ul className={utilStyles.list}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={utilStyles.listItem} key={id}>
+							{title}
+							<br />
+							{id}
+							<br />
+							{date}
+						</li>
+					))}
+				</ul>
 			</section>
 		</Layout>
 	);
